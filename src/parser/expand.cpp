fa3b5ce316c469a1a81c356817464db9c34503e5
CohenArthur <arthur.cohen@epita.fr>
2019-12-26 18:12:26 +0100

    [expand] Add boilerplate for var expansion

--- src/parser/expand.cpp
+++ src/parser/expand.cpp
@@ -1,14 +1,36 @@
+#include <err.h>
 #include <string>
 #include <iostream>
 
 #include "parser.hpp"
 #include "expansion/expansion.hpp"
 
+#define E_FIND_POS(Start_pattern, End_pattern, Retval) \
+    pos_interest = str.find(Start_pattern); \
+    if (pos_interest != string::npos) { \
+        *start_pos = pos_interest; \
+        *end_pos = str.substr(pos_interest, str.size()).find(End_pattern); \
+        \
+        if (*end_pos == string::npos) \
+            return E_ERROR; \
+        \
+        return Retval; \
+    } \
+
 using namespace std;
 
-string find_expandable(string str)
+enum expansion_types
 {
-    (void) str; //FIXME: (void)
+    E_NONE,
+    E_ERROR,
+    E_BRACKETS,
+    E_PARENTHESIS,
+    E_SINGLE,
+};
+
+enum expansion_types find_expandable(string str, size_t *start_pos,
+                                     size_t *end_pos)
+{
     /**
      * There are three ways to expand variables in Makefiles:
      *      - $V
@@ -26,19 +48,64 @@ string find_expandable(string str)
      * they are pretty basic and without condition.
      */
 
-    return "";
+    size_t pos_interest = string::npos;
+
+    E_FIND_POS("$(", ')', E_PARENTHESIS);
+    E_FIND_POS("${", '}', E_BRACKETS);
+
+    if ((*start_pos = str.find('$') != string::npos)) {
+        if (str.at(*start_pos + 1) == '$')
+            return E_SINGLE;
+
+        *end_pos = *start_pos + 2;
+        return E_SINGLE;
+    }
+
+    return E_NONE;
 }
 
-static void handle_map(unordered_map<string, string> *map)
+static string value_expand_all(string value, Parser *p)
 {
-    for (auto& [key, value] : *map) {
-        (*map)[key] = Expansion::expand_all(value, "$$", "$");
-        cout << key << "=" << value << endl;
-    }
+    value = Expansion::expand_all(value, "$$", "$");
+
+    enum expansion_types exp = E_NONE;
+    size_t start_pos;
+    size_t end_pos;
+
+    do {
+        enum expansion_types exp = find_expandable(value, &start_pos, &end_pos);
+        string exp_key;
+
+        switch (exp) {
+        case E_SINGLE:
+            exp_key = value.substr(start_pos + 1, end_pos);
+            break;
+        case E_PARENTHESIS:
+        case E_BRACKETS:
+            exp_key = value.substr(start_pos + 2, end_pos);
+            break;
+        case E_ERROR:
+            errx(2, "parsing error when expanding %s: missing ending character",
+                 value.c_str());
+        case E_NONE:
+            break;
+        }
+
+        value = Expansion::expand_all(value, value.substr(start_pos, end_pos),
+                                      (*p->p_vars)[exp_key]);
+    } while (exp != E_NONE);
+
+    return value;
+}
+
+static void handle_map(unordered_map<string, string> *map, Parser *p)
+{
+    for (const auto& [key, value] : *map)
+        (*map)[key] = value_expand_all(value, p);
 }
 
 void Parser::expand_vars()
 {
-    handle_map(p_vars);
-    handle_map(p_rules);
+    handle_map(p_vars, this);
+    handle_map(p_rules, this);
 }

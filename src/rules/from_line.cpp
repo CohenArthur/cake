981f8a0dc2a71e975342a1e4325223c92bea8d81
CohenArthur <arthur.cohen@epita.fr>
2019-12-26 01:06:45 +0100

    [rules] Fix from_line() test

--- src/rules/from_line.cpp
+++ src/rules/from_line.cpp
@@ -6,5 +6,10 @@ using namespace std;
 
 Rule::RuleType Rule::from_line(string line)
 {
-    return RuleType(line, line);
+    size_t colon_pos = line.find(':');
+
+    string name = line.substr(0, colon_pos);
+    string deps = line.substr(colon_pos + 1, line.size());
+
+    return RuleType(name, deps);
 }
--- tests/test_rules.cpp
+++ tests/test_rules.cpp
@@ -3,8 +3,8 @@
 
 Test(rule, line_valid)
 {
-    Rule valid = Rule::from_line("name: deps");
+    Rule::RuleType valid = Rule::from_line("name: deps");
 
     cr_assert_eq(valid.get_name(), "name");
-    cr_assert_eq(valid.get_deps(), "deps");
+    cr_assert_eq(valid.get_deps(), " deps");
 }
